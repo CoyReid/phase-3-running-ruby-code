19700f812a433430fbdcd1a4ff63909ea8d70e09
Coy Reid <coyreid7@gmail.com>
2021-08-29 12:58:13 -0500

    commit tree 5d4186422613151d053108ca103f851d770ec820

---
+++ app.rb
@@ -0,0 +1,26 @@
+# #this is a comment in ruby starting with a hashtag
+# puts "Hello world!"
+
+# #can also use parenthesis
+# puts("Hello world! with parenthesis")
+
+# #puts adds a line break, print does not
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# #using print does not have line break
+# print "Hello World!"
+# print "Hello World!"
+# print "Hello World!"
+
+# #using p is like using puts.something.inspect
+# puts [1,2,3].inspect
+# p [1,2,3]
+
+# #then there is pretty printing, calls the .pretty_inspect method on data
+# pp [{id: 1, hello: "world"}, {id: 2, hello:"ruby"}, {id: 3, hello:"moon"}, {id:4, hello: "learner"}]
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
